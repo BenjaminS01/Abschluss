aeb418d83135f31985e5915cdd2ea99ca0cea820
BenjaminS01 <bswarovsky@gmx.net>
2021-08-21 17:25:05 +0200

    Newsletter .Net Eureka

---
+++ IT-Kom/Newsletter/WebApplication1/WebApplication1/Migrations/20210821143713_AddNewsModelToDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Newsletter.Migrations
+{
+    public partial class AddNewsModelToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyProperty",
+                columns: table => new
+                {
+                    newsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    titel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    autor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyProperty", x => x.newsId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyProperty");
+        }
+    }
+}
---
+++ IT-Kom/Newsletter/WebApplication1/WebApplication1/dbContexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newsletter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Newsletter.dbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+                
+        }
+        public DbSet<News> MyProperty { get; set; }
+    }
+}
