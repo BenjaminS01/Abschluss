642d3502722a97e2be70d51f9ac1e18e2c789497
BenjaminS01 <bswarovsky@gmx.net>
2021-09-05 22:18:50 +0200

    register works with frontend

--- IT-Kom/Accountverwaltung/src/main/java/com/example/Accountverwaltung/Controller/AccountController.java
+++ IT-Kom/Accountverwaltung/src/main/java/com/example/Accountverwaltung/Controller/AccountController.java
@@ -9,36 +9,41 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@RestController
+@Controller
 @RequestMapping("/account")
 public class AccountController {
 
     @Autowired
     AccountService accountService;
 
-    @PostMapping("/create")
-    public ResponseEntity<CreateAccountResponseModel> createAccont(@Valid @RequestBody CreateAccountRequestModel accountDeatails){
+    @PostMapping("/register")
+    public String createAccont(@Valid  CreateAccountRequestModel accountDetails, Model model){
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
-        AccountDto accountDto = modelMapper.map(accountDeatails, AccountDto.class);
+        AccountDto accountDto = modelMapper.map(accountDetails, AccountDto.class);
         AccountDto createdAccount = accountService.createAccount(accountDto);
 
         CreateAccountResponseModel returnValue = modelMapper.map(createdAccount, CreateAccountResponseModel.class);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
+        return "redirect:/account/register";
     }
 
-    @GetMapping("/show")
-    @ResponseBody
-    public String show() {
+    @GetMapping("/register")
+    public String register( Model model) {
 
-        return "show";
+        model.addAttribute("title", "register");
+
+        model.addAttribute("accountDetails", new CreateAccountRequestModel());
+
+        return "register";
 
     }
 
